148bf2f93101f2c063b1a2869e9d4836ac873bff
Heather Turner <ht@heatherturner.net>
2005-02-18 10:13:04 +0000

    try moving notImplemted.R
    git-svn-id: file:///home/hturner/gnm@14 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/notImplemented.R
+++
@@ -1,8 +0,0 @@
-add1.gnm <- addterm.gnm <- alias.gnm <- anova.gnm <- boxcox.gnm <-
-    confint.gnm <- dfbeta.gnm <- dfbetas.gnm <- drop1.gnm <-
-    dropterm.gnm <- dummy.coef.gnm <- effects.gnm <- kappa.gnm <-
-    influence.gnm <- logtrans.gnm <- predict.gnm <- proj.gnm  <-
-    rstudent.gnm <- function(...) {
-        stop(gsub(".gnm", "", as.character(match.call())[[1]]),
-             " is not implemented for gnm objects")
-    }

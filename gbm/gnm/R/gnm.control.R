23e38389aecd6a4ce2d015b3d95d88e565b9531a
David Firth <davidf@veryspeedy.net>
2005-02-25 15:24:51 +0000

    in gnm.control.R, changed the default to startit = 2
    git-svn-id: file:///home/hturner/gnm@38 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/gnm.control.R
+++ gbm/gnm/R/gnm.control.R
@@ -1,4 +1,4 @@
-gnm.control <- function(epsilon = 1e-4, startit = 10, maxit = 500,
+gnm.control <- function(epsilon = 1e-4, startit = 2, maxit = 500,
                          trace = FALSE) {
     if (!is.numeric(maxit) || maxit <= 0) 
         stop("maximum number of iterations must be > 0")

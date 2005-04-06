71939866567382afda53cf820cec13e6b842dab0
Heather Turner <ht@heatherturner.net>
2005-04-06 13:49:20 +0000

    added conformity data set
    git-svn-id: file:///home/hturner/gnm@87 4c16e084-14e8-0310-9088-c52a9735df6d

---
+++ gbm/gnm/data/conformity.R
@@ -0,0 +1,3 @@
+conformity <- read.table("conformity.txt",
+                         colClasses = c("character", "numeric", "numeric",
+                         "factor", "factor", rep("numeric", 6)))

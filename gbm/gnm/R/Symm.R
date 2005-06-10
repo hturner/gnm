185efc66660a7efab1d211f900282b28972ca7f4
Heather Turner <ht@heatherturner.net>
2005-06-10 08:24:43 +0000

    gtools now separate package so updated error message in Symm.R
    git-svn-id: file:///home/hturner/gnm@131 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/Symm.R
+++ gbm/gnm/R/Symm.R
@@ -1,6 +1,6 @@
 Symm <- function(...){
     if (!require(gtools))
-        stop("requires the gtools package in the gregmisc bundle from CRAN")
+        stop("the gtools package from CRAN needs to be installed")
     dots <- list(...)
     if (any(diff(sapply(dots, length)) != 0)) stop(
                 "arguments to symm() must all have same length")

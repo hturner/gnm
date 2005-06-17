ecb0dae749867f78b92f55665ce60d0a009c514e
Heather Turner <ht@heatherturner.net>
2005-06-17 12:35:23 +0000

    modified termPredictors.default.R to allow NA coefs
    git-svn-id: file:///home/hturner/gnm@137 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/termPredictors.default.R
+++ gbm/gnm/R/termPredictors.default.R
@@ -1,9 +1,9 @@
 termPredictors.default <- function(object, ...) {
     if (is.null(object$termPredictors)){
         X <- model.matrix(object)
-        termPredictors <- t(rowsum(t(X %*% diag(coef(object))),
+        termPredictors <- t(rowsum(t(X %*% diag(naToZero(coef(object)))),
                                     attr(X, "assign")))
-        colnames(termPredictors) <- c("(Intercept)"[attr(X, "assign") == 0],
+        colnames(termPredictors) <- c("(Intercept)"[0 %in% attr(X, "assign")],
                                        attr(object$terms, "term.labels"))
         termPredictors
     }

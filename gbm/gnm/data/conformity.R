7047f9f25b0a09525481c6f41a7f9f8648b8bf33
Heather Turner <ht@heatherturner.net>
2005-06-22 16:02:11 +0000

    minor changes to avoid warnings in RCMD CHECK
    git-svn-id: file:///home/hturner/gnm@145 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/data/conformity.R
+++
@@ -1,3 +0,0 @@
-conformity <- read.table("conformity.txt",
-                         colClasses = c("character", "numeric", "numeric",
-                         "factor", "factor", rep("numeric", 6)))

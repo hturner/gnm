b214827a100fbb6936a6a822a43725a708b3fa71
David Firth <davidf@veryspeedy.net>
2005-12-08 10:34:50 +0000

    Revised residSVD.R and House2001.Rd, and added residSVD.Rd
    git-svn-id: file:///home/hturner/gnm@230 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/residSVD.R
+++ gbm/gnm/R/residSVD.R
@@ -1,18 +1,29 @@
 residSVD <- function(model, fac1, fac2, d = 1) {
     if (!is.null(model$call$data)) {
-        Data <- eval(model$call$data, parent.frame())
+        Data <- as.data.frame(eval(model$call$data, parent.frame()))
         fac1 <- eval(match.call()$fac1, Data)
         fac2 <- eval(match.call()$fac2, Data)
     }
+    if (!inherits(model, "glm") && class(model) != "lm") stop(
+                                        "model not of class lm, glm or gnm")
+    if (!is.factor(fac1)) stop("fac1 must be a factor")
+    if (!is.factor(fac2)) stop("fac2 must be a factor")
     Data <- data.frame(fac1, fac2)
-    if (!is.null(model$na.action)) Data <- Data[-model$na.action,]
-    X <- data.frame(rw = model$residuals * model$weights, w = model$weights)
+    if (!is.null(model$na.action)) Data <- Data[-model$na.action, ]
+    weights <- if (!is.null(model$weights)) as.vector(model$weights) else 1
+    X <- data.frame(rw = as.vector(model$residuals) * weights, w = weights)
     X <- lapply(X, tapply, Data, sum, simplify = TRUE)
-    X <- X$rw/X$w
+    X <- X$rw/mean(na.omit(as.vector(X$w)))
     X <- svd(naToZero(X), d, d)
-    result <- rbind(t(sqrt(X$d[seq(d)]) * t(X$u)), t(sqrt(X$d[seq(d)]) * t(X$v)))
+    uPart <- sqrt(X$d[seq(d)]) * t(X$u)
+    vPart <- sqrt(X$d[seq(d)]) * t(X$v)
+#    uPartNegative <- apply(uPart, 1, function(row) all(row < 0))
+#    vPartNegative <- apply(vPart, 1, function(row) all(row < 0))
+#    multiplier <- ifelse(uPartNegative + vPartNegative == 1, -1, 1)
+    multiplier <-  1
+    result <- t(cbind(uPart, vPart) * multiplier)
     rownames(result) <- c(paste("fac1", levels(fac1), sep = "."),
-                         paste("fac2", levels(fac2), sep = "."))
+                          paste("fac2", levels(fac2), sep = "."))
     colnames(result) <- 1:d
     drop(result)
 }

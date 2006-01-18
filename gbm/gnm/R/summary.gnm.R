3636eb00e151b333ba2e424833483b085fa4d53c
David Firth <davidf@veryspeedy.net>
2006-01-18 15:12:51 +0000

    Amended summary.gnm.R to take account of NA values (other than parameters constrained in the gnm call) in the vector of coefficients.  This works only for objects with no "vcov" component: a corresponding change is needed in gnmFit so that the right thing is done also when vcov=TRUE is specified in the call.
    git-svn-id: file:///home/hturner/gnm@249 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/summary.gnm.R
+++ gbm/gnm/R/summary.gnm.R
@@ -1,11 +1,11 @@
 summary.gnm <- function (object, dispersion = NULL, correlation = FALSE,
-                          symbolic.cor = FALSE, ...) 
+                          symbolic.cor = FALSE, ...)
 {
-    if (is.null(dispersion)) { 
-        if (any(object$family$family == c("poisson", "binomial"))) 
+    if (is.null(dispersion)) {
+        if (any(object$family$family == c("poisson", "binomial")))
             dispersion <- 1
         else if (object$df.residual > 0) {
-            if (any(object$weights == 0)) 
+            if (any(object$weights == 0))
                 warning("observations with zero weight ",
                         "not used for calculating dispersion")
             dispersion <- sum(object$weights * object$residuals^2)/
@@ -13,16 +13,16 @@ summary.gnm <- function (object, dispersion = NULL, correlation = FALSE,
         }
         else dispersion <- Inf
     }
-    if (!"vcov" %in% names(object)){
-        constrain <- object$constrain
+    if (!("vcov" %in% names(object))){
         eliminate <- object$eliminate
-        needToElim <- seq(sum(!constrain[seq(eliminate)]))[eliminate > 0]
-        X <- model.matrix(object)[, !constrain, drop = FALSE]
+        na <- is.na(coef(object)) ## either constrained or aliased
+        needToElim <- seq(sum(!na[seq(eliminate)]))[eliminate > 0]
+        X <- model.matrix(object)[, !na, drop = FALSE]
         Info <- crossprod(X, as.vector(object$weights) * X)
-        if (sum(constrain) > 0) {
-            cov.unscaled <- array(0, dim = rep(length(constrain), 2),
+        if (sum(na) > 0) {
+            cov.unscaled <- array(0, dim = rep(length(na), 2),
                               dimnames = rep(list(names(coef(object))), 2))
-            cov.unscaled[!constrain, !constrain] <-
+            cov.unscaled[!na, !na] <-
                 MPinv(Info, eliminate = needToElim, onlyNonElim = FALSE)
         }
         else
@@ -34,7 +34,7 @@ summary.gnm <- function (object, dispersion = NULL, correlation = FALSE,
     cov.scaled <- dispersion * cov.unscaled
     ans <- c(object[c("call", "terms", "family", "deviance", "aic",
                       "df.residual", "iter")],
-             list(deviance.resid = residuals(object, type = "deviance"), 
+             list(deviance.resid = residuals(object, type = "deviance"),
                   coefficients = coef(object), dispersion = dispersion,
                   cov.unscaled = cov.unscaled, cov.scaled = cov.scaled))
     if (correlation & object$rank > 0) {

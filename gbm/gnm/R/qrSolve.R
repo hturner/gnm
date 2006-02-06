7b593f10230ec62ac384933d4c6d88aefa7c5042
David Firth <davidf@veryspeedy.net>
2006-02-06 19:08:58 +0000

    Tidying up od qrSolve.R, which now allows a matrix for b (and returns the Moore-Penrose inverse of A if no b is specified).
    git-svn-id: file:///home/hturner/gnm@267 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/qrSolve.R
+++ gbm/gnm/R/qrSolve.R
@@ -1,30 +1,38 @@
-qrSolve <- function(A, b, theRank = NULL) {
+qrSolve <- function(A, b, theRank = NULL, ...) {
     ## Function to solve Ax = b, giving the minimum norm solution
     ## (which corresponds to use of the Moore-Penrose inverse) in
     ## the non-full-rank case
     mA <- nrow(A)
     nA <- ncol(A)
+    bMissing <- FALSE
+    if (missing(b)){
+        if (nA != mA) stop("only square matrices can be inverted")
+        b <- diag(nA)
+        bMissing <- TRUE
+    }
     widthA <- min(mA, nA)
     qrA <- qr(A)
     rA <- if (is.null(theRank)) qrA$rank else theRank
     ## Full-rank case:
-    rA <- qrA$rank
     if (rA == widthA) return(solve(qrA, b))
     ## Non-full-rank case: using the complete orthogonal decomposition
     ## (see for example lecture notes by James P Reilly, at
     ##    http://www.ece.mcmaster.ca/~reilly/ee731/ch10.ps
     ## )
-    ##
-    ## But this is acutally slower (because qr is slower than chol)
-    ## for symmetric A, than is MPinv(..., method = "chol")
     Rtrans <- t(qr.R(qrA)[1:rA, ]) ## foot of Reilly's p7
     reversed <- rA:1
     qr2 <- qr(Rtrans[, reversed])
     Ttrans <- qr.R(qr2)[reversed, reversed] ## so it's lower triangular
-  #  Ttrans <- qr.R(qr2)
     Ztrans <- t(qr.Q(qr2)[, reversed])
-    c <- crossprod(qr.Q(qrA)[, 1:rA], b)
-    w <- forwardsolve(Ttrans, c, transpose = TRUE)
-    y <- rep(0, widthA - rA)
-    crossprod(Ztrans, w)[order(qrA$pivot)]
+    cmat <- crossprod(qr.Q(qrA)[, 1:rA], b)
+    w <- forwardsolve(Ttrans, cmat, transpose = TRUE)
+    result <- crossprod(Ztrans, w)[order(qrA$pivot), ]
+    attr(result, "rank") <- rA
+    if (is.matrix(result)) {
+        rownames(result) <- colnames(A)
+        colnames(result) <- if (bMissing) rownames(A)
+        else if (is.matrix(b)) colnames(b)
+        else names(b)
+    } else names(result) <- colnames(A)
+    result
 }

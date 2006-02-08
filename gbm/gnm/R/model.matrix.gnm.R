e17b1ce629d265085ad0fba4760fce0c3bfae512
Heather Turner <ht@heatherturner.net>
2006-02-08 12:26:37 +0000

    added examplesToTime file and attempted to fix bug in model.matrix.gnm() - not tested yet
    git-svn-id: file:///home/hturner/gnm@281 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/model.matrix.gnm.R
+++ gbm/gnm/R/model.matrix.gnm.R
@@ -1,8 +1,13 @@
 model.matrix.gnm <- function(object, ...) {
-    if (!"x" %in% names(object))
-        return(update(object, formula = formula(object),
-                      constrain = object$constrain,
-                      method = "model.matrix", start = coef(object),
-                      verbose = FALSE, trace = FALSE))
+    if (!"x" %in% names(object)) {
+        xCall <- update(object, formula = formula(object),
+                        constrain = object$constrain,
+                        method = "model.matrix", start = coef(object),
+                        verbose = FALSE, trace = FALSE, evaluate = FALSE)
+        env <- environment(formula(object))
+        if (is.null(env)) 
+            env <- parent.frame()
+        eval(xCall, env)
+    }
     object[[match("x", names(object))]]
 }

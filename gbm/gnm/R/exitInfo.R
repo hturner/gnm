e79038864a48df009d0706eab874a8deb1323ac3
Heather Turner <ht@heatherturner.net>
2006-02-24 15:42:40 +0000

    CRAN version 0.8-1
    git-svn-id: file:///home/hturner/gnm@307 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/exitInfo.R
+++ gbm/gnm/R/exitInfo.R
@@ -1,7 +1,7 @@
 exitInfo <- function(object){
     conv <- object$converged
     if (conv)
-        cat("Algorithm converged")
+        cat("Algorithm converged\n")
     else {
         cat("\nTolerance: ", object$tolerance, "\n")
         cat("\nAbsolute scores >= ",

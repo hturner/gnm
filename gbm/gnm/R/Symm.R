cff3e141aff5becdb21eb3d8909be3c13486907f
Heather Turner <ht@heatherturner.net>
2006-03-10 14:08:52 +0000

    now passes checkUsagePackage
    git-svn-id: file:///home/hturner/gnm@316 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/Symm.R
+++ gbm/gnm/R/Symm.R
@@ -3,12 +3,7 @@ Symm <- function(...){
     if (any(diff(sapply(dots, length)) != 0)) stop(
                 "arguments to symm() must all have same length")
     dots <- lapply(dots, as.factor)
-    Levels <- sort(unique(unlist(lapply(dots, levels))))
     facMatrix <- sapply(dots, as.character)
-    nLevels <- length(Levels)
-    combs <- gtools:::combinations(nLevels, length(dots), Levels,
-                                   repeats.allowed = TRUE)
-#    resultLevels <- apply(combs, 1, function(row) paste(row, collapse = ""))
     result <- factor(apply(facMatrix, 1,
                            function(row){
                                string <- paste(sort(row), collapse = "")

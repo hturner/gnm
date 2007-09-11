e61e91754a5aa674add7884f491a6f89c1a2cb76
Heather Turner <ht@heatherturner.net>
2007-09-11 09:48:54 +0000

    Submitted version 0.9-3.
    Fixed bug in DrefWeights() so that it works when the parameters of a weight are constrained to something other than zero.
    
    Updated vignette to include DrefWeights() and added DrefWeights() to NAMESPACE.
    
    git-svn-id: file:///home/hturner/gnm@411 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/DrefWeights.R
+++ gbm/gnm/R/DrefWeights.R
@@ -1,8 +1,10 @@
 DrefWeights <- function(model) {
-    constrain <- pickCoef(model, "delta1")
-    ind <- setdiff(pickCoef(model, "delta[2-9]"), constrain)
-    if (any(!checkEstimable(model, diag(seq(along = coef(model)))[,ind]))) {
-        message("Refitting with first Dref weight constrained to zero")
+    ind <- pickCoef(model, "delta[1-9]")
+    if (any(!checkEstimable(model, diag(seq(along = coef(model)))[,ind]),
+        na.rm = TRUE)){
+        message("Refitting with parameters of first Dref weight constrained ",
+                "to zero")
+        constrain <- pickCoef(model, "delta1")
         model <- update(model, constrain = constrain, start = coef(model),
                         trace = FALSE, verbose = FALSE)
     }
@@ -23,20 +25,17 @@ DrefWeights <- function(model) {
     nw <- length(preds)
     nmod <- nrow(X)
     delta <- matrix(parameters(model)[ind], nmod)
-    ind <- c(t(matrix(ind, nmod, nw - 1)))
+    ind <- c(t(matrix(ind, nmod, nw)))
     vcovDelta <- vcov(model)[ind, ind, drop = FALSE]
-    wc <- unname(1 + rowSums(exp(X %*% delta)))^(-1)
+    wc <- 1/rowSums(exp(X %*% delta))
     wu <- exp(X %*% delta)*wc
-    XX <- matrix(apply(X, 2, rep, nw - 1), nrow(X))
+    XX <- matrix(apply(X, 2, rep, nw), nrow(X))
     out <- list()
-    d <- -wc * (1 - wc) * XX
-    se <- sqrt(rowSums(d %*% vcovDelta * d))
-    out[[1]] <- drop(cbind(dat, weight = wc, se = se))
-    for (i in 1:(nw - 1)) {
+    for (i in 1:nw) {
         d <- -wu[,i] * wu
         d <- c(wu[,i] * (col(wu) == i) + d) * XX
         se <- sqrt(rowSums(d %*% vcovDelta * d))
-        out[[i + 1]] <- drop(cbind(dat, weight = wu[,i], se = se))
+        out[[i]] <- drop(cbind(dat, weight = wu[,i], se = se))
     }
     names(out) <- as.character(preds)
     out

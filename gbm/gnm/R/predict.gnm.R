aa6661f3e39805f66129dac36e3703e0364a0a6e
Heather Turner <ht@heatherturner.net>
2008-03-21 22:40:01 +0000

    Predict.gnm now fully functional for gnm objects when gnmFit used - not so when glm.fit used.
    git-svn-id: file:///home/hturner/gnm@416 4c16e084-14e8-0310-9088-c52a9735df6d

--- gbm/gnm/R/predict.gnm.R
+++ gbm/gnm/R/predict.gnm.R
@@ -4,7 +4,6 @@ predict.gnm <- function (object, newdata = NULL,
                          ...) {
     type <- match.arg(type)
     na.act <- object$na.action
-    object$na.action <- NULL
     if(missing(newdata)) {
         pred <- switch(type, link = object$predictors,
                        response = object$fitted.values,
@@ -23,45 +22,49 @@ predict.gnm <- function (object, newdata = NULL,
         if (!is.null(offID <- attr(modelTerms, "offset")))
             offset <- eval(attr(modelTerms, "variables")[[offID + 1]],
                            newdata)
-        else if (!is.null(object$offset))
+        else
             offset <- eval(object$call$offset, newdata)
-        modelTools <- gnmTools(modelTerms, modelData, x = FALSE)
+        modelTools <- gnmTools(modelTerms, modelData)
         varPredictors <-modelTools$varPredictors(coef(object))
         pred <- modelTools$predictor(varPredictors, term = type == "terms")
         if (!is.null(offset))  pred <- offset + pred
         switch(type, response = {pred <- family(object)$linkinv(pred)},
                terms = {predc <- sweep(pred, 2, colMeans(termPredictors(fit)))
                         const <- sum(pred[1,]) - sum(predc[1,])
-                        pred <- structure(predc[, -1], constant = const)},
+                        pred <- structure(predc[, -1, drop = FALSE],
+                                          constant = const)},
                link = )
     }
     if (se.fit) {
-        if (missing(newdata)) {
-            V <- vcov(object, dispersion = dispersion)
-            residual.scale <- as.vector(sqrt(attr(V, "dispersion")))
+        V <- vcov(object, dispersion = dispersion)
+        residual.scale <- as.vector(sqrt(attr(V, "dispersion")))
+        if (missing(newdata))
             X <- model.matrix(object)
-            switch(type, link = {se.fit <- sqrt(diag(X %*% tcrossprod(V, X)))},
-                   response = {
-                       eta <- family(object)$linkfun(pred)
-                       dX <- family(object)$mu.eta(eta) * X
-                       se.fit <- sqrt(diag(dX %*% tcrossprod(V, dX)))},
-                   terms = {
-                       assign <- setdiff(unique(attr(X, "assign")), 0)
-                       int <- attr(terms(object), "intercept")
+        else
+            X <- modelTools$localDesignFunction(theta, varPredictors)
+        switch(type, link = {se.fit <- sqrt(diag(X %*% tcrossprod(V, X)))},
+               response = {
+                   eta <- family(object)$linkfun(pred)
+                   dX <- family(object)$mu.eta(eta) * X
+                   se.fit <- sqrt(diag(dX %*% tcrossprod(V, dX)))},
+               terms = {
+                   assign <- setdiff(unique(modelTools$termAssign), 0)
+                   int <- attr(terms(object), "intercept")
+                   if (missing(newdata))
                        X <- sweep(X, 2, colMeans(X))
-                       se.fit <- matrix(, nr = nrow(pred), nc = ncol(pred))
-                       for (i in assign + int)
-                           se.fit[,i - int] <- sqrt(diag(X[, i] %*%
-                                                   tcrossprod(V[i, i], X[, i])))
-                   })
-            if ( !is.null(na.act)) {
-                se.fit <- napredict(na.act, se.fit)
-            }
-        }
-        else {
+                   else
+                       X <- sweep(X, 2, colMeans(model.matrix(object)))
+                   se.fit <- matrix(, nr = nrow(pred), nc = ncol(pred))
+                   for (i in assign + int)
+                       se.fit[,i - int] <-
+                           sqrt(diag(X[, i] %*% tcrossprod(V[i, i], X[, i])))
+               })
+        if ( !is.null(na.act)) {
+            se.fit <- napredict(na.act, se.fit)
         }
         dimnames(se.fit) <- dimnames(pred)
-        pred <- list(fit = pred, se.fit = se.fit, residual.scale = residual.scale)
+        pred <- list(fit = pred, se.fit = se.fit,
+                     residual.scale = residual.scale)
     }
     pred
 }

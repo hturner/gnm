4bc6339aa8a418f566e15ae0350190bd35ed054d
David Firth <davidf@veryspeedy.net>
2008-05-16 13:31:19 +0000

    New version of getContrasts.R, and corresponding change to help file getContrasts.Rd.  This version handles only one set of parameters, rather than giving the possibility of specifying several at once.  It also has the new "refLevel" argument, which allows any one of the specified coefs or any weighted average of them to be specified as the reference level for contrasts.  Not yet thoroughly checked.  Does this work for all of our examples?  DF
    git-svn-id: file:///home/hturner/gnm@421 4c16e084-14e8-0310-9088-c52a9735df6d

--- gnm/gnm/R/getContrasts.R
+++ gnm/gnm/R/getContrasts.R
@@ -1,88 +1,94 @@
-getContrasts <- function(model, sets = NULL,
-                         nSets = 1,
+getContrasts <- function(model, set = NULL,
+                         refLevel = "first",
                          dispersion = NULL,
                          use.eliminate = TRUE,
                          ...){
-    result.as.list <- {
-        (is.null(sets) && (nSets > 1)) ||
-        (!is.null(sets) && is.list(sets))
-    }
     coefs <- coef(model)
     l <- length(coefs)
     of.interest <- ofInterest(model)
     if (is.null(of.interest)) {
-        if (l == model$eliminate) stop("model has no parameters of interest")
+        if (l == model$eliminate) stop("Model has no parameters of interest")
         of.interest <- (model$eliminate + 1):l
     }
     coefNames <- names(coefs)
-    if (is.null(sets)) {
+    if (is.null(set)) {
         of.interest <- ofInterest(model)
         if (is.null(of.interest)) {
             if (l == model$eliminate)
-                stop("model has no parameters of interest")
+                stop("Model has no parameters of interest")
             of.interest <- (model$eliminate + 1):l
         }
-        sets <- relimp:::pickFrom(coefNames[of.interest], nSets, ...)
+        set <- unlist(relimp:::pickFrom(coefNames[of.interest], 1, ...))
     }
-    if (!is.list(sets)) sets <- list(sets)
-    setLengths <- sapply(sets, length)
-    if (all(setLengths == 0)) stop(
-            "no non-empty parameter set specified")
-    if (all(setLengths < 1.5)) stop(
-            "for contrasts, at least 2 parameters are needed in a set")
-    if (is.list(sets)) sets <- sets[setLengths > 1.5]
-    if (any(setLengths < 1.5)) warning(
-            "Sets with fewer than 2 parameters were dropped,")
-    sets <- lapply(sets, function(x){
-        if (is.numeric(x)) x <- coefNames[x]
-        contr <- contr.sum(factor(x))
-        rnames <- rownames(contr)
-        contr <- rbind(contr[nrow(contr), ], contr[-nrow(contr), ])
-        rownames(contr) <- rnames
-        list(coefs = x, contr = contr)
+    setLength <- length(set)
+    if (setLength == 0) stop(
+            "No non-empty parameter set specified")
+    if (setLength < 1.5) stop(
+            "For contrasts, at least 2 parameters are needed in a set")
+    if (is.numeric(set)) set <- coefNames[set]
+
+    if (is.numeric(refLevel)){
+        if (length(refLevel) == 0) stop("The specified refLevel has zero length")
+        if (length(refLevel) == 1){
+            if (refLevel %in% seq(setLength)) {
+                temp <- rep(0, setLength)
+                temp[refLevel] <- 1
+                refLevel <- temp
+            } else stop("The specified refLevel is out of range")
+        }
+        if (length(refLevel) != setLength) stop(
+                  "The specified refLevel has the wrong length")
+        if ((sum(refLevel) - 1) ^ 2 > 1e-10) stop(
+                                   "The refLevel weights do not sum to 1")
     }
-                   )
-    coefMatrix <- lapply(sets, function(x){
-        temp <- matrix(0, l, length(x$coefs))
-        id <- match(x$coefs, coefNames)
-        temp[id, 2:ncol(temp)] <- x$contr
-        colnames(temp) <- x$coefs
-        temp})
+    if (identical(refLevel, "first")) refLevel <- c(1, rep(0, setLength - 1)) else
+    if (identical(refLevel, "last")) refLevel <- c(rep(0, setLength - 1), 1) else
+    if (identical(refLevel,"mean")) refLevel <- rep(1/setLength, setLength)
+
+    contr <- diag(rep(1, setLength))
+    contr <- contr - refLevel
+    rownames(contr) <- set
+    set <- list(coefNames = set, contr = contr)
+
+    coefMatrix <- matrix(0, l, length(set$coefNames))
+    id <- match(set$coefNames, coefNames)
+
+    coefMatrix[id, ] <- set$contr
+    colnames(coefMatrix) <- set$coefNames
+
     Vcov <-  vcov(model, dispersion = dispersion,
                   use.eliminate = use.eliminate)
-    result <- lapply(coefMatrix, function(x)
-       {
-        iden <- checkEstimable(model, x)
-        if (any(!na.omit(iden))) {
-            print(iden)
-            cat("Note: not all of the specified contrasts in this set are",
-                "estimable\n")
-        }
-        not.unestimable <- iden | is.na(iden)
-        result <- se(model, x[, not.unestimable, drop = FALSE],
-           checkEstimability = FALSE, Vcov = Vcov)
-        relerrs <- NULL
-        V <- NULL
-        if (any(not.unestimable)){
-            estimable.names <- names(not.unestimable)[not.unestimable]
-            V <- Vcov[estimable.names, estimable.names, drop = FALSE]
-        }
-        if (sum(not.unestimable) > 2) {
-            QVs <- qvcalc:::qvcalc(V)
+
+    iden <- checkEstimable(model, coefMatrix)
+    if (any(!na.omit(iden))) {
+        cat("Estimability of the specified contrasts:\n")
+        print(iden)
+        if (all(!na.omit(iden))) stop(
+                 "None of the specified contrasts is estimable")
+        cat("Note: not all of the specified contrasts in this set are",
+            "estimable\n")
+    }
+    not.unestimable <- iden | is.na(iden)
+    result <- se(model, coefMatrix[, not.unestimable, drop = FALSE],
+                 checkEstimability = FALSE, Vcov = Vcov)
+    relerrs <- NULL
+    V <- NULL
+    if (any(not.unestimable)){
+        estimable.names <- names(not.unestimable)[not.unestimable]
+        V <- Vcov[estimable.names, estimable.names, drop = FALSE]
+    }
+    if (sum(not.unestimable) > 2) {
+        QVs <- qvcalc:::qvcalc(V)
             quasiSE <- sqrt(QVs$qvframe$quasiVar)
             result <- cbind(result, quasiSE)
             names(result)[1:2] <- c("estimate", "SE")
             result$quasiVar <- QVs$qvframe$quasiVar
             relerrs <- QVs$relerrs
-
         }
-        return(structure(list(covmat = V,
-                              qvframe = result,
-                              relerrs = relerrs,
-                              modelcall = model$call),
-                         class = "qv")
-               )
-    }
+    return(structure(list(covmat = V,
+                          qvframe = result,
+                          relerrs = relerrs,
+                          modelcall = model$call),
+                     class = "qv")
            )
-    if (result.as.list) result else result[[1]]
 }

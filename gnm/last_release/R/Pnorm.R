f99170fbf267fe4cf30a7f70bc245feed3b6fbba
Heather Turner <ht@heatherturner.net>
2009-08-11 13:38:39 +0000

    version 0.10-0 (without Plogis and Pnorm)
    git-svn-id: file:///home/hturner/gnm@452 4c16e084-14e8-0310-9088-c52a9735df6d

--- gnm/last_release/R/Pnorm.R
+++
@@ -1,9 +0,0 @@
-Pnorm <- Phi <- function(x, inst = NULL){
-    list(variables = list(substitute(x)),
-         term = function(predLabels, varLabels) {
-             paste("pnorm(", varLabels, ")", sep = "")
-         },
-         call = as.expression(match.call()),
-         match = 1)
-}
-class(Pnorm) <- class(Phi) <- "nonlin"

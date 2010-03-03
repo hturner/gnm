5aea3c09364637b01a3ea11086f5338595b437fb
David Firth <davidf@veryspeedy.net>
2010-03-03 19:26:23 +0000

    Changes to checkEstimable -- determination of linear dependence is now by rankMatrix (from the Matrix package) rather than by qr.  This seems more reliable, if very slightly slower.
    git-svn-id: file:///home/hturner/gnm@467 4c16e084-14e8-0310-9088-c52a9735df6d

--- gnm/last_release/R/checkEstimable.R
+++ gnm/last_release/R/checkEstimable.R
@@ -1,24 +1,26 @@
-checkEstimable <- function(model, combMatrix = NULL,
-                           tolerance = 1e6 * .Machine$double.eps)
+checkEstimable <- function(model,
+                            combMatrix = diag(length(coef(model))),
+                            tolerance = NULL)
 {
     if (!inherits(model, "gnm")) stop("model not of class gnm")
     coefs <- coef(model)
-    X <- model.matrix(model)
-    l <- ncol(X)
-    if (!l) return(logical(0))
-    constrained <- seq(l) %in% model$constrain
-    X <- X[, !constrained, drop = FALSE]
-    if (is.null(combMatrix))
-        combMatrix <- diag(seq_len(l))
-    else combMatrix <- as.matrix(combMatrix)
+    l <- length(coefs)
+    combMatrix <- as.matrix(combMatrix)
     if (nrow(combMatrix) != l) stop(
           "dimensions of combMatrix do not match coef(model)")
-    combMatrix <- scale(combMatrix[!constrained, ], center = FALSE)
-    resultNA <- is.na(colSums(combMatrix))
-    resids <- qr.resid(qr(crossprod(X)), combMatrix[, !resultNA, drop = FALSE])
-    rms <- apply(resids, 2, sd)
-    result <- rep.int(NA, length(resultNA))
-    result[!resultNA] <- rms < tolerance
+    X <- model.matrix(model)[, !is.na(coefs), drop = FALSE]
+    combMatrix <- scale(combMatrix[!is.na(coefs), ], center = FALSE)
+    resultNA <- apply(combMatrix, 2, function(col) any(is.na(col)))
+    result <- logical(ncol(combMatrix))
+    is.na(result) <- resultNA
+    rankX <- rankMatrix(X)
+    check.1 <- function(comb){
+        Xc <- rbind(X, comb)
+        rankXc <- rankMatrix(Xc, tol = tolerance)
+        return(rankXc == rankX)
+    }
+    result[!resultNA] <- apply(combMatrix[, !resultNA, drop = FALSE],
+                               2, check.1)
     names(result) <- colnames(combMatrix)
     return(result)
 }

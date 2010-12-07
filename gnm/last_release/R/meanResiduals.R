1e06591fa077947a824490143888b9071afa4acc
Heather Turner <ht@heatherturner.net>
2010-12-07 12:18:30 +0000

    tidied up meanResiduals function, methods and doc
    git-svn-id: file:///home/hturner/gnm@489 4c16e084-14e8-0310-9088-c52a9735df6d

--- gnm/last_release/R/meanResiduals.R
+++ gnm/last_release/R/meanResiduals.R
@@ -1,4 +1,4 @@
-##' Computes the mean working residual from a model fitted using
+##' Computes the mean working residuals from a model fitted using
 ##' Iterative Weighted Least Squares for each level of a factor or
 ##' interaction of factors.
 ##'
@@ -7,44 +7,67 @@
 ##' @title Average Residuals within Factor Levels
 ##' @param object model object for which \code{object$residuals} gives the
 ##' working residuals and \code{object$weights} gives the working weights.
-##' @param by a list of factors.
+##' @param by either a formula specifying one or more factors (recommended)
+##' or a list of factors (the elements of which must correspond exactly to
+##' observations in the model frame). When more than one factor is specified,
+##' their interaction is used to specify the grouping factor.
 ##' @param standardized logical: if \code{TRUE}, the mean residuals are
 ##' standardized to be approximately standard normal.
 ##' @param as.table logical: if \code{TRUE} the result is returned as a table
 ##' cross-classified by the factors passed to \code{by}.
 ##' @param ... currently ignored
-##' @return If \code{as.table == TRUE}, the mean residuals cross-classified
-##' by the factors passed to \code{by}, otherwise a vector of mean residuals.
-##' In either case the returned object has a single attribute,
-##' \code{"weights"} which gives the weight associated with each
-##' grouped residual.
+##' @return An object of class \code{"meanResiduals"}, for which \code{print}
+##' and \code{summary} methods are provided. A \code{"meanResiduals"}
+##' object is a list containing the following elements:
+##' \item{ call }{ the call used to create the model object from which the
+##' mean residuals are derived. }
+##' \item{ by }{ a label for the grouping factor. }
+##' \item{ residuals }{ the mean residuals. }
+##' \item{ df }{ the degrees of freedom associated with the mean
+##' residuals. }
+##' \item{ standardized }{ the \code{standardized} argument. }
 ##' @author Heather Turner
 ##' @example examples/meanResiduals.R
 meanResiduals <- function(object, by = NULL, standardized = TRUE, as.table = TRUE, ...){
-  r <- object$residuals
-  ## recompute weights for better accuracy
-  w  <- as.numeric(object$prior.weights * object$family$mu.eta(predict(object, type = "link"))^2/
-                   object$family$variance(object$fitted))
-  if (is.null(by))
-    stop("`by' must be specified in order to compute grouped residuals")
-  ## find factors as in mosaic.glm
-  by <- do.call("model.frame", list(formula = by,
-            data = object$data, subset = object$call$subset, na.action = na.pass))
-  agg.wts <- tapply(w, by, sum) #unlike rowsum, keeps all levels of interaction
-  res <- tapply(r * w, by, sum)/agg.wts
-  if (standardized) res <- res * sqrt(agg.wts)
-  if (!as.table){
-    res <- structure(c(res), weights = c(agg.wts))
-  }
-  else
-    res <- structure(as.table(res), weights = as.table(agg.wts))
-  ## now compute degrees of freedom
-  fac <- interaction(by) # drop levels?
-  Xreduced <- rowsum(model.matrix(object), fac, na.rm = TRUE)
-  res <- list(call = object$call, by = match.call()$by, residuals = res,
-              df = nlevels(fac) - rankMatrix(Xreduced))
-  class(res) <- "meanResiduals"
-  return(res) # chi-squared test?
+    if (is.null(by))
+        stop("`by' must be specified in order to compute grouped residuals")
+    if (inherits(by, "formula")){
+        ## find factors as in mosaic.glm
+        by <- do.call("model.frame",
+                      list(formula = by, data = object$data, subset = object$call$subset,
+                           na.action = na.pass, drop.unused.levels = TRUE))
+        ## following loop needed due to bug in model.frame.default (fixed for R 2.12)
+        for(nm in names(by)) {
+            f <- by[[nm]]
+            if(is.factor(f) && length(unique(f[!is.na(f)])) < length(levels(f)))
+                by[[nm]] <- by[[nm]][, drop = TRUE]
+        }
+        if (!is.null(object$na.action))
+            by <- by[-object$na.action,]
+    }
+    fac <- factor(interaction(by)) # drop unused levels
+    if (length(fac) != length(object$y))
+        stop("Grouping factor of length", length(fac),
+             "but model frame of length", length(object$y))
+
+    r <- object$residuals
+    ## recompute weights for better accuracy
+    w  <- as.numeric(object$prior.weights * object$family$mu.eta(predict(object, type = "link"))^2/
+                     object$family$variance(object$fitted))
+    agg.wts <- tapply(w, by, sum) #unlike rowsum, keeps all levels of interaction
+    res <- tapply(r * w, by, sum)/agg.wts
+    if (standardized) res <- res * sqrt(agg.wts)
+    if (!as.table){
+        res <- structure(c(res), weights = c(agg.wts))
+    }
+    else
+        res <- structure(as.table(res), weights = as.table(agg.wts))
+    ## now compute degrees of freedom
+    Xreduced <- rowsum(model.matrix(object), fac, na.rm = TRUE)
+    res <- list(call = object$call, by = paste(names(by), collapse = ":"), residuals = res,
+                df = nlevels(fac) - rankMatrix(Xreduced), standardized = standardized)
+    class(res) <- "meanResiduals"
+    return(res)
 }
 
 
--- gnm/last_release/R/print.meanResiduals.R
+++ gnm/last_release/R/print.meanResiduals.R
@@ -1,21 +1,8 @@
+# this should just print residuals as default (could be as table)
 print.meanResiduals <- function (object, digits = max(3, getOption("digits") - 3), ...) {
-    cat("\nModel call:\n", deparse(object$call), "\n", sep = "", fill = TRUE)
+    cat("\nModel call:\n", deparse(object$call, width = options()$width),
+        "\n", sep = "", fill = TRUE)
 
-    cat("Mean residuals by", deparse(object$by),  ":\n\n")
-    if (length(object$residuals > 20)) {
-        object$residuals <- quantile(object$residuals, na.rm = TRUE)
-        names(object$residuals) <- c("Min", "1Q", "Median", "3Q",
-                                     "Max")
-    }
-    print.default(object$residuals, digits = digits, na = "", print.gap = 2)
-
-    cat("\nTest of Normality:\n")
-    if (object$df > 0) {
-        chi.sq <- sum(as.vector(object$residuals)^2)
-        p.value <- pchisq(chi.sq, object$df, lower.tail = FALSE)
-        test <- c(chi.sq, object$df, p.value)
-        cat("\nChi^2 =", format(chi.sq, digits = digits), "on",
-            object$df, "df, p-value =", format(p.value, digits = digits), "\n")
-    }
-    else cat("\n(zero degrees of freedom)\n")
+    cat("Mean residuals by ", object$by,  ":\n\n", sep = "")
+    print(object$residuals, digits = digits, print.gap = 2)
 }

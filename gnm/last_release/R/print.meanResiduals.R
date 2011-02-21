5e2f8e2ff0682a6e7eaa41f6312eb3e8e68843d2
Heather Turner <ht@heatherturner.net>
2011-02-21 22:33:51 +0000

    tidied up gnm ready for new release
    git-svn-id: file:///home/hturner/gnm@496 4c16e084-14e8-0310-9088-c52a9735df6d

--- gnm/last_release/R/print.meanResiduals.R
+++ gnm/last_release/R/print.meanResiduals.R
@@ -1,8 +1,8 @@
 # this should just print residuals as default (could be as table)
-print.meanResiduals <- function (object, digits = max(3, getOption("digits") - 3), ...) {
-    cat("\nModel call:\n", deparse(object$call, width = options()$width),
+print.meanResiduals <- function (x, digits = max(3, getOption("digits") - 3), ...) {
+    cat("\nModel call:\n", deparse(x$call, width = options()$width),
         "\n", sep = "", fill = TRUE)
 
-    cat("Mean residuals by ", object$by,  ":\n\n", sep = "")
-    print(object$residuals, digits = digits, print.gap = 2)
+    cat("Mean residuals by ", x$by,  ":\n\n", sep = "")
+    print(x$residuals, digits = digits, print.gap = 2)
 }

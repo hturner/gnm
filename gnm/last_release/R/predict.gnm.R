d0c523585a61445b9f543fba4bf7615c786623f5
Heather Turner <ht@heatherturner.net>
2015-04-20 16:12:34 +0000

    version of predict.gnm including eliminate when type = "terms"
    git-svn-id: file:///home/hturner/gnm@520 4c16e084-14e8-0310-9088-c52a9735df6d

--- gnm/last_release/R/predict.gnm.R
+++ gnm/last_release/R/predict.gnm.R
@@ -1,4 +1,4 @@
-#  Copyright (C) 2005, 2008, 2010, 2012, 2014 Heather Turner
+#  Copyright (C) 2005, 2008, 2010, 2012, 2014, 2015 Heather Turner
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -24,7 +24,7 @@ predict.gnm <- function (object, newdata = NULL,
             terms <- c("(eliminate)"[!is.null(object$eliminate)],
                        attr(object$terms, "term.labels"))
     }
-    if(missing(newdata)) {
+    if (missing(newdata)) {
         pred <- switch(type, link = object$predictors,
                        response = object$fitted.values,
                        terms = {pred <- termPredictors(object)
@@ -41,26 +41,47 @@ predict.gnm <- function (object, newdata = NULL,
         }
         if (!inherits(pred, "matrix") && !is.null(object$table.attr))
             attributes(pred) <- object$table.attr
-    }
-    else {
+    } else {
         modelTerms <- delete.response(terms(object))
-        modelData <- model.frame(modelTerms, newdata, na.action = na.action,
-                                  xlev = object$xlevels)
-        if (length(offID <- attr(modelTerms, "offset")))
+        ## evaluate eliminate in environment of formula
+        if (is.null(object$eliminate)){
+            modelData <- model.frame(modelTerms, newdata, na.action = na.action,
+                                     xlev = object$xlevels)
+        } else {
+            modelData <-
+                model.frame(modelTerms, newdata, eliminate = eval(eliminate),
+                            na.action = na.action, xlev = object$xlevels)
+        }
+        ## use same contrasts as in original model
+        contr <- lapply(model.frame(object)[names(modelData)],
+                        attr, "contrasts")
+        for (i in which(!sapply(contr, is.null))){
+            modelData[[i]] <- C(modelData[[i]], contr[[i]])
+        }
+        if (length(offID <- attr(modelTerms, "offset"))){
             offset <- eval(attr(modelTerms, "variables")[[offID + 1]],
                            newdata)
-        else
-            offset <- eval(object$call$offset, newdata)
+        } else offset <- eval(object$call$offset, newdata)
         modelTools <- gnmTools(modelTerms, modelData)
         varPredictors <- modelTools$varPredictors(parameters(object))
         pred <- modelTools$predictor(varPredictors, term = type == "terms")
         names(pred) <- rownames(modelData)
         if (!is.null(offset))  pred <- offset + pred
+        if (!is.null(object$eliminate)) {
+            prede <- attr(coef(object), "eliminate")
+            switch(type, terms = {pred <-
+                cbind(`(eliminate)` = prede[modelData$`(eliminate)`], pred)},
+                   pred <- prede[modelData$`(eliminate)`] + pred)
+        }
         switch(type, response = {pred <- family(object)$linkinv(pred)},
-               terms = {predc <- sweep(pred, 2, colMeans(termPredictors(object)))
-                        const <- sum(pred[1,]) - sum(predc[1,])
-                        pred <- structure(predc[, terms, drop = FALSE],
-                                              constant = const)},
+               terms = {if (hasintercept) {
+                            predc <- sweep(pred, 2,
+                                           colMeans(termPredictors(object)))
+                            const <- sum(pred[1,]) - sum(predc[1,])
+                            pred <- structure(predc[, terms, drop = FALSE],
+                                              constant = const)
+                        } else structure(pred[, terms, drop = FALSE],
+                                                 constant = 0)},
                link = )
         if (!is.null(na.act <- attr(modelData, "na.action")))
             pred <- napredict(na.act, pred)
@@ -68,46 +89,62 @@ predict.gnm <- function (object, newdata = NULL,
     if (se.fit) {
         V <- vcov(object, dispersion = dispersion, with.eliminate = TRUE)
         residual.scale <- as.vector(sqrt(attr(V, "dispersion")))
-        if (missing(newdata))
+        if (missing(newdata)) {
             X <- model.matrix(object)
-        else
+            elim <- object$eliminate
+        } else {
             X <- modelTools$localDesignFunction(parameters(object),
                                                 varPredictors)
-        covElim <- attr(V, "covElim")[object$eliminate, , drop = FALSE]
+            elim <- modelData$`(eliminate)`
+        }
+        covElim <- attr(V, "covElim")[elim, , drop = FALSE]
+        varElim <- attr(V, "varElim")[elim]
         switch(type,
                link = {
-                   if (is.null(object$eliminate))
+                   if (is.null(elim))
                        se.fit <- sqrt(diag(X %*% tcrossprod(V, X)))
                    else se.fit <-
                        sqrt(diag(X %*% tcrossprod(V, X)) +
-                                2 * rowSums(X * covElim) +
-                                    attr(V, "varElim")[object$eliminate])},
+                                2 * rowSums(X * covElim) + varElim)},
                response = {
                    eta <- na.omit(c(family(object)$linkfun(pred)))
                    d <- family(object)$mu.eta(eta)
                    if (is.null(object$eliminate))
                        se.fit <- sqrt(diag(X %*% tcrossprod(V, X)))
                    else se.fit <- sqrt(diag(X %*% tcrossprod(V, X)) +
-                                       2*rowSums(X * covElim) +
-                                       attr(V, "varElim")[object$eliminate])
+                                       2*rowSums(X * covElim) + varElim)
                    se.fit <- d * se.fit},
                terms = {
                    if (missing(newdata)) {
                        assign <- split(seq(ncol(X)), attr(X, "assign"))
-                       X <- sweep(X, 2, colMeans(X))
-                   }
-                   else {
+                   } else {
                        M <- model.matrix(object)
                        assign <- split(seq(ncol(X)), attr(M, "assign"))
-                       X <- sweep(X, 2, colMeans(M))
+                   }
+                   if (hasintercept) {
+                       if (missing(newdata)) {
+                           X <- sweep(X, 2, colMeans(X))
+                       } else X <- sweep(X, 2, colMeans(M))
                    }
                    se.fit <- matrix(, nrow = nrow(X), ncol = length(terms))
                    s <- 0
+                   adj <- hasintercept - !is.null(object$eliminate)
                    for (i in match(terms, colnames(pred))) {
                        s <- s + 1
-                       t <- assign[[i]]
-                       se.fit[, s] <-
-                           sqrt(diag(X[, t] %*% tcrossprod(V[t, t], X[, t])))
+                       if (terms[i] != "(eliminate)") {
+                           t <- assign[[i + adj]]
+                           ## check estimability of term
+                           comb <- rep.int(0, length(coef(object)))
+                           comb[t] <- 1
+                           estimable <- checkEstimable(object, comb)
+                           if (estimable) {
+                               se.fit[, s] <-
+                                   sqrt(diag(X[, t] %*%
+                                                 tcrossprod(V[t, t], X[, t])))
+                           }
+                       } else {
+                           se.fit[, 1] <- sqrt(varElim)
+                       }
                    }
                })
         if (!is.null(na.act)) {

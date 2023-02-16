5207f923de97ed34167c1be719e1a2582115408d
Heather Turner <ht@heatherturner.net>
2023-02-16 16:06:25 +0000

    use QR vs glm.fit.e to find inestimable linear par

--- R/gnmFit.R
+++ R/gnmFit.R
@@ -1,4 +1,4 @@
-#  Copyright (C) 2005-2013 Heather Turner and David Firth
+#  Copyright (C) 2005-2023 Heather Turner and David Firth
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -44,33 +44,21 @@ gnmFit <-
     nelim <- nlevels(eliminate)
     non.elim <- seq.int(nelim + 1, length(start))
 
-    ## add constraints specified by modelTools and glm
-    tmpTheta <- as.double(rep.int(NA, nTheta))
+    ## add constraints specified by modelTools and inestimable linear parameters
+    tmpTheta <- rep.int(NA_real_, nTheta)
     varPredictors <- modelTools$varPredictors(tmpTheta)
     X <- modelTools$localDesignFunction(tmpTheta, varPredictors)
     isLinear <- unname(!is.na(colSums(X)))
-    tmpTheta[constrain] <- constrainTo
-    unspecified <- unname(is.na(tmpTheta))
-    if (any(isLinear & unspecified)) {
-        tmpTheta[isLinear & unspecified] <-
-            suppressWarnings(glm.fit.e(X[, isLinear & unspecified, 
-                                         drop = FALSE],
-                                       y,
-                                       family = family,
-                                       intercept = FALSE,
-                                       eliminate = if (nelim) eliminate
-                                       else NULL,
-                                       coefonly = TRUE,
-                                       control = glm.control(maxit = 1)))
-
-        extraLin <- which(isLinear & is.na(tmpTheta))
+    QR <- qr(X[, isLinear])
+    if (QR$rank < sum(isLinear)) {
+        extraLin <- which(isLinear)[QR$pivot[-seq_len(QR$rank)]]
     } else extraLin <- numeric()
-
     extra <- setdiff(c(modelTools$constrain, extraLin), constrain)
     ind <- order(c(constrain, extra))
     constrain <- c(constrain, extra)[ind]
     constrainTo <- c(constrainTo, numeric(length(extra)))[ind]
     notConstrained <- !seq.int(nTheta) %in% constrain
+    
     status <- "not.converged"
     unspecifiedNonlin <- FALSE
     dev <- numeric(2)
@@ -120,7 +108,8 @@ gnmFit <-
                           family = family,
                           intercept = FALSE,
                           eliminate = if (nelim) eliminate else NULL,
-                          coefonly = TRUE)})
+                          coefonly = TRUE,
+                          ridge = ridge)})
             ## if no starting values for elim, use result of above
             if (initElim) alpha <- unname(attr(tmpTheta, "eliminated"))
             theta[unspecifiedLin] <- naToZero(tmpTheta)
